1b8310bd3d439ad7fa81ea8c71e0c6a1e6ce0167
haneetsinghchhabra <HOTh@n!!t52p93>
2021-09-24 02:31:08 +0530

    Initial commit

---
+++ app/src/main/java/com/technorapper/kotlinpracsi/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.technorapper.kotlinpracsi
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.activity.viewModels
+
+class MainActivity : AppCompatActivity() {
+    private val viewModel by viewModels<MainActivityViewModel>()
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        viewModel.testSime()
+    }
+}
---
+++ app/src/main/java/com/technorapper/kotlinpracsi/MainActivityViewModel.kt
@@ -0,0 +1,118 @@
+package com.technorapper.kotlinpracsi
+
+import android.util.Log
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.*
+
+import kotlinx.coroutines.flow.collect
+
+
+class MainActivityViewModel : ViewModel() {
+    override fun onCleared() {
+
+        super.onCleared()
+    }
+
+
+    private val _downloading: MutableLiveData<Boolean> = MutableLiveData()
+    val downloading: LiveData<Boolean> = _downloading
+    fun setStateEvent(mainStateEvent: MainStateEvent) {
+        viewModelScope.launch {
+
+
+        }
+
+
+    }
+
+     fun testSime() {
+        CoroutineScope(Dispatchers.IO).launch {
+
+            var resultOneDeferred = async { println("test one ${System.currentTimeMillis()}") }
+            var resultTwoDeferred = async { println("test three ${System.currentTimeMillis()}") }
+            async { println("test two ${System.currentTimeMillis()}") }.await()
+            async { println("test four ${System.currentTimeMillis()}") }.await()
+           // val combinedResult = resultOneDeferred.await() + resultTwoDeferred.await()
+            awaitAll(async {
+                println("test five ${System.currentTimeMillis()}")
+            }, async {
+                println("test six ${System.currentTimeMillis()}")
+            }, async {
+                println("test seven ${System.currentTimeMillis()}")
+            })
+
+            coroutineScope {
+                launch { call1() }
+                launch { call2() }
+                launch { call3() }
+            }
+        }
+        runInParallel()
+        GlobalScope.launch(Dispatchers.IO) { }
+
+        Log.d(TAG, "After all finishings")
+
+    }
+
+    var TAG = "test"
+    suspend fun call1() = withContext(Dispatchers.IO) {
+        Log.d(TAG, "Call 1 started")
+        delay(2000)
+        Log.d(TAG, "Call 1 fiished")
+    }
+
+    suspend fun call2() = withContext(Dispatchers.IO) {
+        Log.d(TAG, "Call 2 started")
+        delay(5000)
+        Log.d(TAG, "Call 2 finished")
+    }
+
+    suspend fun call3() = withContext(Dispatchers.IO) {
+        Log.d(TAG, "Call 3 started")
+        delay(1000)
+        Log.d(TAG, "Call 3 finished")
+    }
+
+    private var job: Job = Job()
+    private var scope =
+        CoroutineScope(Dispatchers.Main + job) // creating the scope to run the coroutine. It consists of Dispatchers.Main (coroutine will run in the Main context) and job to handle the cancellation of the coroutine.
+
+    fun runInParallel() {
+        scope.launch { // launch a coroutine
+            // runs in parallel
+            val deferredList = listOf(
+                scope.async { println("test eight ${System.currentTimeMillis()}") },
+                scope.async { println("test nine  ${System.currentTimeMillis()}") },
+                scope.async { println("test tem ${System.currentTimeMillis()}") },
+                scope.async { println("test tem ${System.currentTimeMillis()}") },
+                scope.async { println("test tem ${System.currentTimeMillis()}") },
+                scope.async { println("test tem ${System.currentTimeMillis()}") },
+                scope.async { println("test tem ${System.currentTimeMillis()}") },
+                scope.async { println("test tem ${System.currentTimeMillis()}") },
+                scope.async { println("test tem ${System.currentTimeMillis()}") },
+                scope.async { println("test tem ${System.currentTimeMillis()}") },
+                scope.async { println("test tem ${System.currentTimeMillis()}") },
+                scope.async { println("test tem ${System.currentTimeMillis()}") },
+                scope.async { println("test tem ${System.currentTimeMillis()}") },
+                scope.async { println("test tem ${System.currentTimeMillis()}") },
+                scope.async { println("test tem ${System.currentTimeMillis()}") },
+                scope.async { println("test tem ${System.currentTimeMillis()}") }
+            )
+
+            deferredList.awaitAll() // wait for all data to be processed without blocking the UI thread
+
+            // do some stuff after data has been processed, for example update UI
+        }
+    }
+
+}
+
+sealed class MainStateEvent {
+
+    object FetchImages : MainStateEvent()
+
+}
+
